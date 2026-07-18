221f419d6b2f53acafdd703aeca49442522c1efe
Pat Rogers <progers@classwide.com>
2026-07-18 09:50:41 -0500

    Adding new concurrency folder and Blocking package

---
+++ src/concurrency/blocking.adb
@@ -0,0 +1,296 @@
+--
+--  Copyright (C) 2026 Patrick Rogers
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+--  Author: Patrick Rogers, progers@classwide.com
+
+with Ada.Dynamic_Priorities;     use Ada.Dynamic_Priorities;
+
+package body Blocking is
+
+   ------------------------
+   -- Counting_Semaphore --
+   ------------------------
+
+   protected body Counting_Semaphore is
+
+      entry Acquire when Count > 0 is
+      begin
+         Count := Count - 1;
+      end Acquire;
+
+      procedure Release is
+      begin
+         Count := Count + 1;
+      end Release;
+
+   end Counting_Semaphore;
+
+   ----------------------
+   -- Binary_Semaphore --
+   ----------------------
+
+   protected body Binary_Semaphore is
+
+      entry Acquire when Available is
+      begin
+         Available := False;
+      end Acquire;
+
+      procedure Release is
+      begin
+         Available := True;
+      end Release;
+
+   end Binary_Semaphore;
+
+   -----------
+   -- Mutex --
+   -----------
+
+   protected body Mutex is
+
+      entry Acquire when Available is
+      begin
+         Available := False;
+         Current_Owner := Acquire'Caller;
+      end Acquire;
+
+      procedure Release is
+      begin
+         if Current_Owner = Current_Task then
+            Available := True;
+            Current_Owner := Null_Task_Id;
+         else
+            raise Protocol_Error;
+         end if;
+      end Release;
+
+      procedure Reset is
+      begin
+         Available := True;
+         Current_Owner := Null_Task_Id;
+      end Reset;
+
+   end Mutex;
+
+   ------------------------------
+   -- Priority_Extending_Mutex --
+   ------------------------------
+
+   protected body Priority_Extending_Mutex is
+
+      entry Acquire when Available is
+      begin
+         Available := False;
+         Current_Owner := Acquire'Caller;
+         Owner_Base_Priority := Get_Priority (Current_Owner);
+         Set_Priority (Ceiling, Current_Owner);
+         --  The caller's base priority will now retain, after the call to
+         --  Acquire returns, the elevated value it inherited (ie Ceiling).
+         --  This takes effect after the end of the protected action.
+      end Acquire;
+
+      procedure Release is
+      begin
+         if Current_Owner = Current_Task then
+            Set_Priority (Owner_Base_Priority, Current_Owner);
+            --  The base priority will go back to the original value it had
+            --  when the caller called Acquire. This takes effect after the
+            --  end of the protected action.
+            Available := True;
+            Current_Owner := Null_Task_Id;
+         else
+            raise Protocol_Error;
+         end if;
+      end Release;
+
+      procedure Reset is
+      begin
+         Available := True;
+         Current_Owner := Null_Task_Id;
+      end Reset;
+
+   end Priority_Extending_Mutex;
+
+   ----------------------
+   --  Reentrant_Mutex --
+   ----------------------
+
+   protected body Reentrant_Mutex is
+
+      entry Acquire when True is
+      begin
+         if Current_Owner = Null_Task_Id then
+            Current_Owner := Acquire'Caller;
+            Depth := 1;
+         elsif Current_Owner = Acquire'Caller then
+            Depth := Depth + 1;
+         else -- held already, but not by current caller
+            requeue Retry with abort;
+         end if;
+      end Acquire;
+
+      procedure Release is
+      begin
+         if Current_Owner = Current_Task then
+            Depth := Integer'Max (0, Depth - 1);
+            if Depth = 0 then
+               Current_Owner := Null_Task_Id;
+            end if;
+         else
+            raise Protocol_Error;
+         end if;
+      end Release;
+
+      entry Retry when Depth = 0 is
+      begin
+         Depth := 1;
+         Current_Owner := Retry'Caller;
+      end Retry;
+
+   end Reentrant_Mutex;
+
+   -----------
+   -- Event --
+   -----------
+
+   protected body Event is
+
+      procedure Set is
+      begin
+         Current_State := Up;
+      end Set;
+
+      procedure Reset is
+      begin
+         Current_State := Down;
+      end Reset;
+
+      procedure Toggle is
+      begin
+         if Current_State = Up then
+            Current_State := Down;
+         else
+            Current_State := Up;
+         end if;
+      end Toggle;
+
+      entry Wait (S : Event_States) when True is
+      begin
+         if S = Up then
+            requeue Wait_Up with abort;
+         else
+            requeue Wait_Down with abort;
+         end if;
+      end Wait;
+
+      function State return Event_States is
+      begin
+         return Current_State;
+      end State;
+
+      entry Wait_Up when Current_State = Up is
+      begin
+         null;
+      end Wait_Up;
+
+      entry Wait_Down when Current_State = Down is
+      begin
+         null;
+      end Wait_Down;
+
+   end Event;
+
+   -----------------------
+   -- Persistent_Signal --
+   -----------------------
+
+   protected body Persistent_Signal is
+
+      procedure Send is
+      begin
+         Signal_Arrived := True;
+      end Send;
+
+      entry Wait when Signal_Arrived is
+      begin
+         Signal_Arrived := False;
+      end Wait;
+
+   end Persistent_Signal;
+
+   -------------------
+   -- Pulsed_Signal --
+   -------------------
+
+   protected body Pulsed_Signal is
+
+      procedure Send is
+      begin
+         if Wait'Count > 0 then
+            Signal_Arrived := True;
+         end if;
+      end Send;
+
+      entry Wait when Signal_Arrived is
+      begin
+         Signal_Arrived := False;
+      end Wait;
+
+   end Pulsed_Signal;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   overriding procedure Initialize (This : in out Scope_Lock) is
+   begin
+      This.Lock.Acquire;
+   end Initialize;
+
+   --------------
+   -- Finalize --
+   --------------
+
+   overriding procedure Finalize (This : in out Scope_Lock) is
+   begin
+      This.Lock.Release;
+   end Finalize;
+
+   --------------------------------
+   -- Readers_Writers_Controller --
+   --------------------------------
+
+   protected body Readers_Writers_Controller is
+
+      entry Request_Reading when not Writer_Present is
+      begin
+         Current_Readers := Current_Readers + 1;
+      end Request_Reading;
+
+      procedure Stop_Reading is
+      begin
+         Current_Readers := Current_Readers - 1;
+      end Stop_Reading;
+
+      entry Request_Writing when not Writer_Present is
+      begin
+         Writer_Present := True;
+         requeue Start_Writing;
+      end Request_Writing;
+
+      entry Start_Writing when Current_Readers = 0 is
+      begin
+         null;
+      end Start_Writing;
+
+      procedure Stop_Writing is
+      begin
+         Writer_Present := False;
+      end Stop_Writing;
+
+   end Readers_Writers_Controller;
+
+end Blocking;
---
+++ src/concurrency/blocking.ads
@@ -0,0 +1,216 @@
+--
+--  Copyright (C) 2026 Patrick Rogers
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+--  Author: Patrick Rogers, progers@classwide.com
+
+--  This package provides mechanisms for concurrent programming. Most are not in
+--  the SPARK subset so SPARK_Mode is left in Auto mode.
+
+with Ada.Finalization;
+with Ada.Task_Identification; use Ada.Task_Identification;
+with System;
+
+package Blocking is
+
+   Protocol_Error : exception;
+   --  Raised when client tasks do not follow the protocol required by the
+   --  given blocking abstraction. For example, if the abstraction requires
+   --  that the task that acquired a lock be the task that releases it.
+
+   ------------------------------  Signals  -----------------------------------
+
+   --  A type allowing one task to "signal" some other task to allow the other
+   --  task to continue execution. The signal is retained until a caller calls
+   --  Wait, i.e., it is retained whether or not a caller is currently waiting
+   --  for it.
+
+   protected type Persistent_Signal is
+      procedure Send;
+      entry Wait;
+   private
+      Signal_Arrived : Boolean := False;
+   end Persistent_Signal;
+
+   --  A type allowing one task to send a "signal" some other task to allow
+   --  the other task to continue execution. When a signal arrives, it is NOT
+   --  retained if no caller is currently waiting for it.
+
+   protected type Pulsed_Signal is
+      procedure Send;
+      entry Wait;
+   private
+      Signal_Arrived : Boolean := False;
+   end Pulsed_Signal;
+
+   ----------------------------  Semaphores  ----------------------------------
+
+   --  A basic counting semaphore for concurrent programming without regard to
+   --  priorities.
+
+   protected type Counting_Semaphore (Initial_Count : Natural) is
+      entry Acquire;
+      procedure Release;
+   private
+      Count : Natural := Initial_Count;
+   end Counting_Semaphore;
+
+   --  A basic binary semaphore for concurrent programming without regard to
+   --  priorities.
+
+   protected type Binary_Semaphore (Initially_Available : Boolean) is
+      entry Acquire;
+      procedure Release;
+   private
+      Available : Boolean := Initially_Available;
+   end Binary_Semaphore;
+
+   ------------------------------  Mutexes  -----------------------------------
+
+   --  A basic mutex for concurrent programming without regard to priorities.
+
+   protected type Mutex is
+      entry Acquire;
+      procedure Release;
+      --  Release raises Protocol_Error if the caller is not the current owner
+      procedure Reset;
+      --  Takes the object back to the initial elaborated state
+   private
+      Available     : Boolean := True;
+      Current_Owner : Task_Id := Null_Task_Id;
+   end Mutex;
+
+   --  A mutex type that provides priority inheritance like that of an
+   --  OS-defined mutex.
+   --
+   --  See https://www.adacore.com/blog/priority-extending-mutexes for a
+   --  detailed discussion.
+
+   protected type Priority_Extending_Mutex
+     (Ceiling : System.Priority)
+   with
+      Priority => Ceiling
+   is
+
+      entry Acquire;
+      --  The caller's base priority is set to the value of Ceiling and the
+      --  caller is the "current owner" of the mutex.
+
+      procedure Release;
+      --  The mutex lock is relinquished and the caller's base priority is set
+      --  back to the value it had at the point of the call to Acquire. Raises
+      --  Protocol_Error if the current owner is not the task calling Release
+
+      procedure Reset;
+      --  Takes the object back to the initial elaborated state
+
+   private
+      Available           : Boolean := True;
+      Current_Owner       : Task_Id := Null_Task_Id;
+      Owner_Base_Priority : System.Priority;
+   end Priority_Extending_Mutex;
+
+   --  A mutex type that allows the current holder of a given mutex to
+   --  "re-acquire" that mutex without blocking.
+
+   protected type Reentrant_Mutex is
+
+      entry Acquire;
+      --  Acquires the mutex if available. Blocks the caller if another
+      --  thread already owns the mutex. However, if the current caller
+      --  already owns the mutex that fact is noted and the caller
+      --  continues without blocking.
+
+      procedure Release;
+      --  Relinquishes the mutex if the number of calls to Release matches the
+      --  number of prior calls to Seize. Raises Protocol_Error if the caller
+      --  is not the current holder of the mutex.
+
+   private
+
+      entry Retry;
+      --  Internal target of requeue when the mutex is already owned.
+
+      Depth : Natural := 0;
+      --  Number of calls to Seize for a given holder. A value of zero
+      --  indicates than no task currently holds the mutex.
+
+      Current_Owner : Task_Id := Null_Task_Id;
+      --  The current holder of the mutex, initially none.
+
+   end Reentrant_Mutex;
+
+   -----------------------------  Scope Locks  ----------------------------------
+
+   --  A lock that automatically acquires the referenced Binary_Semaphore
+   --  on initialization and releases it during finalization. Simply declare an
+   --  object of the Scope_Lock type within a declarative part to ensure mutual
+   --  exclusive access to all the data and routines declared within that
+   --  declarative part. The Lock itself (the discriminant value) must be
+   --  declared outside of the declarative part in which the Scope_Lock object
+   --  is located.
+   --
+   --  For example:
+   --
+   --       S : Binary_Semaphore;
+   --       X : Obj; -- data to be protected from race conditions
+   --
+   --       procedure Update_X_1 is
+   --          Lock : Scope_lock (S'Access);
+   --       begin
+   --           -- update X without race conditions...
+   --       end Update_X_1;
+   --
+   --       procedure Update_X_2 is
+   --          Lock : Scope_lock (S'Access);
+   --       begin
+   --           -- update X without race conditions...
+   --       end Update_X_2;
+
+   type Scope_Lock (Lock : access Binary_Semaphore) is tagged limited private;
+
+   -----------------------------  Readers/Writers Protocol  ----------------------------------
+
+   --  A type providing the readers-writers access protocol.
+
+   protected type Readers_Writers_Controller is
+      entry Request_Reading;
+      procedure Stop_Reading;
+      entry Request_Writing;
+      procedure Stop_Writing;
+   private
+      entry Start_Writing;
+      Current_Readers : Natural := 0;
+      Writer_Present  : Boolean := False;
+   end Readers_Writers_Controller;
+
+   -------------------------------  Events  -----------------------------------
+
+   --  The states an application task can specify for the Event type below.
+   type Event_States is (Up, Down);
+
+   --  A type providing "events" that tasks can set (and clear and toggle),
+   --  and await.
+
+   protected type Event (Initial_State : Event_States := Down) is
+      procedure Set    with Post => State = Up;
+      procedure Reset  with Post => State = Down;
+      procedure Toggle with Post => State /= State'Old;
+      entry Wait (S : Event_States);
+      function State return Event_States;
+   private
+      entry Wait_Up;
+      entry Wait_Down;
+      Current_State : Event_States := Initial_State;
+   end Event;
+
+private
+
+   type Scope_Lock (Lock : access Binary_Semaphore) is
+      new Ada.Finalization.Limited_Controlled with null record;
+
+   overriding procedure Initialize (This : in out Scope_Lock);
+   overriding procedure Finalize (This : in out Scope_Lock);
+
+end Blocking;
